915344469a8c80bf9f3731be41ab309df8d58c2a
Rafael Albano <rafaelb.albano@gmail.com>
2022-05-03 15:22:41 -0300

    Criação do Widget do aplicativo.

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          500: '#54d959'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
